45e8872178e154dd2929fc33be59c4bb867498b1
Veerababu-254 <veerella.veerababu@gmail.com>
2025-05-26 21:10:52 +0530

    added comments

--- ClinetScripts/8859_payment_batch_selection_cs.js
+++ ClinetScripts/8859_payment_batch_selection_cs.js
@@ -21,7 +21,7 @@ define([
         //Refactoring to SS2.1
         var record;
         var subListId = 'custpage_8859_batch_list_open';
-// comments are added in file.
+// comments are added in file to repo
         //Payment Batch status
         var BATCH_OPEN = '1';
         var BATCH_UPDATING = '2';
